890ab4f5961e41e5f8fea7959bb5c30ddb58a470
DevSusu <os1742@gmail.com>
2015-02-13 15:06:07 -0500

    delete unused django app

--- crimes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from . import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.crime_collection),
-    url(r'^(?P<crime_id>[0-9]+)/$', views.crime_detail),
-)

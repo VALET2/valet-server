d3f9ca8c170c8b335de54529763c4585ddc86917
DevSusu <os1742@gmail.com>
2015-02-18 19:49:07 -0500

    Fixed Prediction Mode. now filters data from the original

--- static/js/prediction.js
+++ static/js/prediction.js
@@ -4,71 +4,8 @@ $(document).ready( function(){
 
   $('#compare-img-map').on('click', function(){
 
-    if ( $('#prediction-map').length != 0) {
-      $('#map-canvas').removeClass('col-md-6').addClass('col-md-12');
-      $('#prediction-map').parent().remove();
-      google.maps.event.trigger(map, 'resize');
-      map.setCenter(new google.maps.LatLng(40.418641, -86.892279));
-      map.setZoom(13);
-
-      $('.prediction-panel').removeClass('col-md-offset-4');
-      $('.prediction-panel').removeClass('col-md-4');
-      $('.prediction-panel').addClass('col-md-3');
-
-      // $('.disable-on-prediction').prop('disabled',false);
-      $('.hide-on-prediction').show();
-
-      searchData.startDate = searchBeforePrediction.startDate;      
-      searchData.endDate = searchBeforePrediction.endDate;
-
-
-      getData(function(data){ 
-        onDataChange(data);
-      });
-      onDataChange(getData());
-      
-    }
-    else {
-      var year = predictionDate.year();
-      var month = predictionDate.month()+1;
-      var date = predictionDate.date();
-      var hour = predictionDate.get('hour');
-      var minute = predictionDate.get('minute');
-
-      searchData.police = []
-      searchData.crimeType = []
-      searchBeforePrediction = $.extend(true, {}, searchData);
-      searchData.startDate = predictionDate;
-      searchData.endDate = predictionDate;
-
-      getData(function(data){
-        onDataChange(data);
-      });
-
-      if( month < 10 ) month = "0" + month;
-      if( date < 10 ) date = "0" + date;
-      if( hour < 10 ) hour = "0" + hour;
-      if( minute < 10 ) minute = "0" + minute;
-
-      // Predict_MM-DD-YYYY_HHmm-HHmm_Cat1.jpg
-      var dateString = 'Predict_' + month + '-' + date + '-' + year + '_' + hour + minute + ".jpg";
-      var img = $('<div class="col-md-6"><img class="img-responsive" src="' + "/static/prediction/" + dateString + '" id="prediction-map"/></div>');
-
-      mapHtml = $('#map-canvas');
-      mapHtml.removeClass("col-md-12").addClass('col-md-6');
-      mapHtml.parent().append(img);
-
-      $('.prediction-panel').addClass('col-md-offset-4').addClass('col-md-4').removeClass('col-md-3');
-
-      google.maps.event.trigger(map, "resize");
-      map.setCenter(new google.maps.LatLng(40.418641, -86.892279));
-      map.setZoom(12);
-      $("#prediction-map").css("height", $('#map-canvas').height())
-
-      // $('.disable-on-prediction').prop('disabled',true);
-      $('.hide-on-prediction').hide();
-
-    }
+    loadDataForPrediction();
+    renderView();
 
   });
 
@@ -76,34 +13,119 @@ $(document).ready( function(){
   predictionDate.set('minute',00);
 
   $("#input-prediction-date").click(function(e){
-      searchData.startDate = predictionDate;
-      searchData.endDate = predictionDate;
+    
+    changeSearchData({
 
-      var year = predictionDate.year();
-      var month = predictionDate.month()+1;
-      var date = predictionDate.date();
-      var hour = predictionDate.get('hour');
-      var minute = predictionDate.get('minute');
+        police : [],
+        crimeType : crimeRiskDict[$('input:checked').val()],
+        date : predictionDate
+        // startDate : predictionDate,
+        // endDate : predictionDate
 
-      getData(function(data){
-        onDataChange(data);
+    });
+    console.log("Searching Data...");
+    console.dir(searchData);
+
+    onDataChange(getData());
+    // getData(function(data){
+    //     onDataChange(data);
+    // });
+
+    if ( $('#prediction-map').length == 0 )
+      renderView();
+
+  });
+
+  loadDataForPrediction = function() {
+
+    if ( $('#prediction-map').length != 0) {
+
+      changeSearchData(searchBeforePrediction);
+      console.log("searchBeforePrediction");
+      console.dir(searchBeforePrediction);
+
+    }
+    else {
+
+      changeSearchData({
+
+        police : [],
+        crimeType : crimeRiskDict[$('input:checked').val()],
+        date : predictionDate
+        // startDate : predictionDate,
+        // endDate : predictionDate
+
       });
+      console.dir(searchData);
+      searchBeforePrediction = $.extend(true, {}, searchData);
 
-      if( month < 10 ) month = "0" + month;
-      if( date < 10 ) date = "0" + date;
-      if( hour < 10 ) hour = "0" + hour;
-      if( minute < 10 ) minute = "0" + minute;
+    }
 
-      // Predict_MM-DD-YYYY_HHmm-HHmm_Cat1.jpg
-      var dateString = 'Predict_' + month + '-' + date + '-' + year + '_' + hour + minute + ".jpg";
-      $('#prediction-map').parent().remove();
-      var img = $('<div class="col-md-6"><img class="img-responsive" src="' + "/static/prediction/" + dateString + '" id="prediction-map"/></div>');
+    // filter from the original datas.
+    onDataChange(getData());
+    // getData(function(data){
+    //     onDataChange(data);
+    // });
+  }
 
-      $('#map-canvas').parent().append(img);
-      $("#prediction-map").css("height", $('#map-canvas').height())
+  renderView = function() {
+    var predictionImg = $('#prediction-map');
+    var mapHtml = $('#map-canvas');
 
+    if ( predictionImg.length != 0) {
+      // Going back to normal
+      mapHtml.removeClass('col-md-6').addClass('col-md-12');
+      predictionImg.parent().remove();
+      
+      google.maps.event.trigger(map, 'resize');
+      map.setCenter(new google.maps.LatLng(40.418641, -86.892279));
+      map.setZoom(13);
 
-  })
+      $('.prediction-panel').removeClass('col-md-offset-4').removeClass('col-md-4').addClass('col-md-3');
+      $('.hide-on-prediction').show();
+    }
+    else {
+      // Changing to prediction mode
+      mapHtml.addClass('col-md-6').removeClass('col-md-12');
+
+      var img = $('<div class="col-md-6">' + 
+                  '<img class="img-responsive" src="' + 
+                  "/static/prediction/" + 
+                  getDateInFormat($('input:checked').val()) + '" id="prediction-map" style="height:450px;"/></div>'); 
+      img.css('height', mapHtml.height());
+      mapHtml.parent().append(img);
+
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(new google.maps.LatLng(40.418641, -86.892279));
+      map.setZoom(12);
+
+      $('.prediction-panel').addClass('col-md-offset-4').addClass('col-md-4').removeClass('col-md-3');
+      $('.hide-on-prediction').hide();
+    }
+  }
+
+  // make src string in format Predict_YYYY-MM-DD_HH00_Cat1.jpg
+  getDateInFormat = function(risk) {
+    var year = predictionDate.year();
+    var month = predictionDate.month()+1;
+    var date = predictionDate.date();
+    var hour = predictionDate.get('hour');
+    var minute = predictionDate.get('minute');
+
+    if( month < 10 ) month = "0" + month;
+    if( date < 10 ) date = "0" + date;
+    if( hour < 10 ) hour = "0" + hour;
+    if( minute < 10 ) minute = "0" + minute;
+
+    // Predict_MM-DD-YYYY_HHmm-HHmm_Cat1.jpg
+    return 'Predict_' + month + '-' + date + '-' + year + '_' + hour + minute + "_" + risk + ".jpg";
+  }
+
+  changeSearchData = function(searchDict) {
+    for( var i in searchDict) {
+      searchData[i] = searchDict[i];
+    }
+  };
 
   datePicker = $('.datepicker').datetimepicker({
     defaultDate: predictionDate,
@@ -112,7 +134,5 @@ $(document).ready( function(){
   }).on('dp.change', function(ev){
       predictionDate = moment(ev.date);
   });
-
-
 
 });
